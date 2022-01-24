17cfe948f267df397af7c7cbf9baf437ee53d0a6
Ash-Coder-or-Not <newestideaz@gmail.com>
2022-01-24 18:10:25 +0530

    changed database.sql

--- src/database/database.sql
+++ src/database/database.sql
@@ -1,9 +1,9 @@
 drop database if exists EventManagementSystem;
-create database EventManagementSystem;
+create database Shop;
 use EventManagementSystem;
 
-create table events (
-    id varchar(10) primary key not null,
+create table products (
+    sr_no varchar(10) primary key not null,
     name tinytext not null,
     description text not null,
     event_manager tinytext not null,
@@ -12,3 +12,11 @@ create table events (
     budget int not null,
     location tinytext not null
 );
+
+create table user (
+    user_id varchar(5) primary key not null,
+    name tinytext not null,
+    city tinytext not null,
+    phone_number bigint not null,
+    item_bought int not null
+)
